71255f1579a2593b3a7032e612f63cd97511aa5b
AJBababa <alvarojbustos@gmail.com>
2026-06-15 12:21:14 +0200

    render x2

--- render-start.sh
+++ render-start.sh
@@ -4,10 +4,6 @@ set -e
 
 PORT="${PORT:-10000}"
 
-if [ -z "${APP_KEY}" ]; then
-    export APP_KEY="$(php artisan key:generate --show --no-ansi)"
-fi
-
 mkdir -p \
     storage/logs \
     storage/framework/cache/data \
@@ -21,9 +17,8 @@ chmod -R ug+rwX storage bootstrap/cache
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT}/" /etc/apache2/sites-available/000-default.conf
 
-php artisan config:clear
+php artisan optimize:clear
 php artisan config:cache
-php artisan route:cache
 php artisan view:cache
 
 apache2-foreground
